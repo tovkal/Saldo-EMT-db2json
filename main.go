8261ee67e95f2ab9d9bedc65e8e5a54ce1f1946c
tovkal <Tovkal@gmail.com>
2016-10-14 22:26:40 +0200

    Start building JSON with BusLines

--- main.go
+++ main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+	"bytes"
 	"database/sql"
-	"fmt"
 	"log"
+	"strconv"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -16,19 +17,61 @@ const language_export string = "es"
 func main() {
 	// Create the database handle, confirm driver is present
 	db, err := sql.Open("mysql", db_user+":@"+"/"+db_name)
+	checkError(err)
+
 	defer db.Close()
 
+	var languageId string
+
+	err = db.QueryRow("SELECT id FROM Language WHERE code = ?", language_export).Scan(&languageId)
+	checkError(err)
+
+	var buffer bytes.Buffer
+
+	buffer.WriteString("{")
+
+	buffer.WriteString(getBusLines(db))
+
+	buffer.WriteString("}")
+	log.Println("Resulting JSON:")
+	log.Print(buffer.String())
+}
+
+func checkError(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func getBusLines(db *sql.DB) string {
+	var buffer bytes.Buffer
 
 	rows, err := db.Query("SELECT * FROM BusLine")
+	checkError(err)
+	buffer.WriteString("\"lines\": [")
 
+	firstTime := true
 	defer rows.Close()
 	for rows.Next() {
 		var id int
 		var hexColor string
 		err = rows.Scan(&id, &hexColor)
-		fmt.Printf("id: %v, hexColor: %v\n", id, hexColor)
+		checkError(err)
+
+		if firstTime {
+			firstTime = false
+		} else {
+			buffer.WriteString(",")
+		}
+
+		buffer.WriteString("{")
+		buffer.WriteString("\"" + strconv.Itoa(id) + "\" : { \"color\" : \"" + hexColor + "\"}")
+		//buffer.WriteString("\"" + id + "\" : { \"color\" : \"" + hexColor + "\", \"name\" : " + name + "\"}")
+		buffer.WriteString("}")
+
+		//fmt.Printf("id: %v, hexColor: %v\n", id, hexColor)
 	}
+	buffer.WriteString("]")
+
+	return buffer.String()
 }
