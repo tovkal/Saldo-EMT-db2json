0057d0f929b0bbae5c87963fc4918733d9a91247
tovkal <Tovkal@gmail.com>
2016-10-15 22:21:21 +0200

    Added Fare table to output, still some tables left

--- main.go
+++ main.go
@@ -23,10 +23,11 @@ func main() {
 	defer db.Close()
 
 	// Get language id for value defined in constatnt language_export
-	//languageId := getLanguageId(db)
+	languageId := getLanguageId(db)
 
 	var buffer bytes.Buffer
 	buffer.WriteString("{")
+	buffer.WriteString(getFares(db, languageId))
 	buffer.WriteString(getBusLines(db))
 	buffer.WriteString("}")
 
@@ -58,6 +59,57 @@ func getLanguageId(db *sql.DB) string {
 	checkError(err)
 
 	return languageId
+}
+
+func getFares(db *sql.DB, languageId string) string {
+	var buffer bytes.Buffer
+
+	rows, err := db.Query("SELECT Fare.id, Fare.days, Fare.rides FROM Fare")
+	checkError(err)
+	defer rows.Close()
+	buffer.WriteString("\"fares\": [")
+
+	firstFare := true
+	for rows.Next() {
+		var id int
+		var days sql.NullInt64
+		var rides sql.NullInt64
+
+		err = rows.Scan(&id, &days, &rides)
+		checkError(err)
+
+		if firstFare {
+			firstFare = false
+		} else {
+			buffer.WriteString(",")
+		}
+
+		buffer.WriteString("{\"" + strconv.Itoa(id) + "\": " + "{")
+
+		firstAttribute := true
+
+		if days.Valid {
+			buffer.WriteString("\"days\":" + strconv.Itoa(int(days.Int64)))
+			firstAttribute = false
+		}
+
+		if rides.Valid {
+			if !firstAttribute {
+				buffer.WriteString(",")
+			} else {
+				firstAttribute = false
+			}
+
+			buffer.WriteString("\"rides\":" + strconv.Itoa(int(rides.Int64)))
+		}
+
+		buffer.WriteString("}")
+		buffer.WriteString("}")
+	}
+
+	buffer.WriteString("],")
+
+	return buffer.String()
 }
 
 func getBusLines(db *sql.DB) string {
